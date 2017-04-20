e70bf4a44d9a696707e7db0333353f821584d546
Siddhartha Goyal <sidd@tinyurl.com>
2017-04-19 21:01:36 -0400

    added capability to store history of deployments

--- lambda-create.js
+++ lambda-create.js
@@ -6,53 +6,50 @@ const fs = require('fs');
 const path = require('path');
 
 program
-	.option('-n, --no-upload', 'if set, creation and uploading of a new zip package is skipped')
-	.option('-s, --stage <stage>', 'the stage to deploy to')
-	.option('-p, --profile <profile>', 'the profile to use for deployment')
-	.option('-r, --region <region>', 'the region in which to deploy the function')
+	.option('-n, --skip-upload', 'Skip creating a local zip file and uploading to s3')
+	.option('-s, --stage <stage>', 'The stage alias to assign the upload')
+	.option('-p, --profile <profile>', 'The local profile to use when deploying')
+	.option('-r, --region <region>', 'The region in which to deploy the function')
 	.parse(process.argv);
 
-var stage = program.stage;
+//get the lambda spec path
 var lambdaspec = program.args;
-var region = program.region;
-
 if (!lambdaspec.length) {
 	console.error('No lambda spec given');
 	process.exit(1);
 }
-
 var lambdaspecFullpath = fs.realpathSync(lambdaspec[0]);
 lambdaspec = require(lambdaspecFullpath);
 
 var zipfile = lambdaspec.zipfile;
-var files = lambdaspec.files;
 var s3bucket = lambdaspec.s3bucket;
 var s3keyprefix = lambdaspec.s3keyprefix;
-
-//set the real paths
-var zippath = path.isAbsolute(zipfile) ? zipfile : path.join(process.cwd(), zipfile);
-var filepaths = files.map((glob) => { return path.isAbsolute(glob) ? glob : path.join(process.cwd(), glob); });
 var s3key = s3keyprefix + zipfile;
+
+//set the profile object according to the profile and region settings
 var profile = program.profile ? `--profile ${program.profile}` : "";
 profile += program.region ? ` --region ${program.region}` : "";
 
-//if the no-upload flag is set then we skip over zipping up the contents of the 
-//package and go straight to setting up the lambda function
-if (!program.upload) {
+//if the skip-upload flag is set then we skip over zipping and uploading the package to s3
+if (!program.skipUpload) {
+	
+	var files = lambdaspec.files;
+	var filepaths = files.map((glob) => { return path.isAbsolute(glob) ? glob : path.join(process.cwd(), glob); });
+	var ziplocal = path.isAbsolute(zipfile) ? zipfile : path.join(process.cwd(), zipfile);
+
 	//zip up the distribution files
 	console.log(`Creating lambda function distribution package '${zipfile}' from [${files.join()}]...`);
 	try {
-		execSync(`zip -rq ${zippath} ${filepaths.join(" ")} ${fs.realpathSync('package.json')} ${fs.realpathSync('node_modules')}`);
+		execSync(`zip -rq ${ziplocal} ${filepaths.join(" ")} ${fs.realpathSync('package.json')} ${fs.realpathSync('node_modules')}`);
 	} catch (err) {
 		console.error(`Error zipping file: ${err.message}`);
 		process.exit(1);
 	}
 	
-	
 	//upload the zip file to s3
 	console.log(`Uploading '${zipfile}' to '${s3bucket}' with key '${s3key}'...`);
 	try {
-		execSync(`aws s3api put-object --bucket ${s3bucket} --key ${s3key} --body ${zippath} ${profile}`);
+		execSync(`aws s3api put-object ${profile} --bucket ${s3bucket} --key ${s3key} --body ${ziplocal}`);
 	} catch (err) {
 		console.error(`Error uploading '${zipfile}' to '${s3bucket}': ${err.message}`);
 		process.exit(1);
@@ -63,24 +60,28 @@ if (!program.upload) {
 var lambdaconfig = lambdaspec.lambdaconfig;
 var codeSpec = `S3Bucket=${s3bucket},S3Key=${s3key}`;
 var vpcspec = lambdaspec.vpcconfig ? `--vpc-config SubnetIds=${lambdaspec.vpcconfig.SubnetIds.join()},SecurityGroupIds=${lambdaspec.vpcconfig.SecurityGroupIds.join()}` : "";
+
 console.log(`Creating lambda function '${lambdaconfig.FunctionName}'...`);
-var res = '';
+var createRes = '';
 try {
-	res = execSync(`aws lambda create-function ${profile} --code ${codeSpec} ${vpcspec} --cli-input-json '${JSON.stringify(lambdaconfig)}'`,
-						null, {stdio:['pipe','pipe','ignore']});
+	createRes = execSync(`aws lambda create-function ${profile} --code ${codeSpec} ${vpcspec} --cli-input-json '${JSON.stringify(lambdaconfig)}'`,
+							null, {stdio:['pipe','pipe','ignore']});
 } catch (err) {
 	console.error(`Error creating lambda function: ${err.message}`);
 	process.exit(1);
 }
-res = JSON.parse(res);
-console.log(`Lambda function created with resource ARN '${res.FunctionArn}'`);
+createRes = JSON.parse(createRes);
+console.log(`Lambda function created with resource ARN '${createRes.FunctionArn}'`);
 
-//if a stage is specified then an alias is also created by default this points to the '$LATEST' version
+//if the create was successful and a stage is specified then an alias is also created 
+//the alias will point to the specific version of the lambda function just published
+var stage = program.stage;
 var aliasRes = null;
-if (stage) {
-	console.log(`Creating alias '${stage}' for lambda function '${lambdaconfig.FunctionName}'`);
+if (createRes && stage) {
+	var funcVersion = createRes.Version;
+	console.log(`Creating alias '${stage}' for lambda function '${lambdaconfig.FunctionName}' version '${funcVersion}'`);
 	try {
-		aliasRes = execSync(`aws lambda create-alias ${profile} --function-name ${res.FunctionArn} --name ${stage} --function-version '\$LATEST'`,
+		aliasRes = execSync(`aws lambda create-alias ${profile} --function-name ${createRes.FunctionArn} --name ${stage} --function-version '${funcVersion}'`,
 								null, {stdio:['pipe','pipe','ignore']});
 		aliasRes = JSON.parse(aliasRes);
 		console.log(`Alias created`);		
@@ -90,31 +91,31 @@ if (stage) {
 	}
 }
 
-//if the lambda spec has a 'version' field then
+//if the lambda function was successfully created
 //create a version history object and update it with the information about the deployment
-if (lambdaspec.version && res) {
+if (createRes) {
 	var history = {};
 
-	//first create a 'versions' entry that will as keys whatever version is in the 
-	//lambda spec
-	//the value associated with each key is an array of objects, with each object
-	//containing details about the actual deployment
-	history.versions = {};
-	history.versions[lambdaspec.version] = [];
+	//create a versions entry that is an array of deployments
+	history.versions = [];
 	
 	//create the deployment object
 	var user = execSync(`git config github.user`);
 	var deployment = {
-		version: parseInt(res.Version),
-		date: res.LastModified,
+		lambdaVersion: createRes.Version,
+		moduleVersion: lambdaspec.version,
+		date: createRes.LastModified,
 		user: user.toString().trim()
-	}
-	history.versions[lambdaspec.version].push(deployment);
+	};
+	history.versions.push(deployment);
 	
 	//if there is a stage that is set then add that to the aliases structure
 	if (stage) {
 		history.aliases = {};
-		history.aliases[stage] = aliasRes.FunctionVersion;
+		history.aliases[stage] = {
+			current: aliasRes.FunctionVersion,
+			versions: [createRes.Version]
+		};
 	}
 	
 	//now save the history object to file
