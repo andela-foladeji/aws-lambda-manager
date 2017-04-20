ba928e6ef34d246786d1d49b53d1499e04114028
Siddhartha Goyal <sidd@tinyurl.com>
2017-04-20 11:08:57 -0400

    added capability to create or update a stage and set it to point to a particular version of a lambda function

--- lambda.js
+++ lambda.js
@@ -4,6 +4,7 @@ const program = require('commander');
 
 program
 	.version('0.0.1')
-	.command('create [spec]', 'Creates a new lambda function')
-	.command('update [spec]', 'Updates an existing lambda function')
+	.command('create <spec>', 'Creates a new lambda function')
+	.command('update <spec>', 'Updates an existing lambda function')
+	.command('set-stage <spec> <stage> <version>', 'Sets the stage for a lambda function to point to a particular version')
 	.parse(process.argv);
